48896331ca369c2d5a34ebef86c3ab9f3f02586f
Anish-dev <anishb2041@gmail.com>
2023-10-28 17:33:49 +0530

    fs

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
-import http from "http"
-import {generateLovePercent} from "./features.js" 
+// Child Process
 
-console.log(generateLovePercent())
-const server = http.createServer((req,res) => {
-    if(req.url === "/about"){
-        res.end("<h1>About Page</h1>")
-    }else if (req.url ==="/"){
-        res.end("<h1>Home Page</h1>")
-    } else if(req.url ==="/contact"){
-        res.end("<h1>Contact Page</h1>")
-    }else{
-        res.end("<h1>Page Not Found</h1>")
-    }
-})
+// let cp=require("child_process");
+// cp.execSync("calc")
+// cp.execSync("start chrome https:\\www.youtube.com")
 
-server.listen(5000,() => {
-    console.log("Server is working")
-})
+//OS Features
+
+// let os = require("os")
+// console.log(os.arch());
+// console.log(os.platform());
+// console.log(os.networkInterfaces());
+//  console.log(os.cpus());
+
+// FileSystem
+
+// files =>create, read ,update , delete 
+// let fs = require("fs")
+
+// read
+ // let buffer = fs.readFileSync("abc.js")
+// console.log("bin data"  + buffer)
+
+//create
+// fs.openSync("abc.txt", "w");
+// no file ->create and if  file exist -> content replace
+// fs.writeFileSync("abc.txt", "backend sikh raha hu")
+
+//update
+// fs.appendFileSync("abc.txt", " sikh leya")
+
+
+//Folder
+// create
+// fs.mkdirSync("folder")  create folder 
+// fs.writeFileSync("folder/text.txt","heeee")
+
+//delete file
+// let content = fs.readdirSync("folder");
+// console.log(content);
+// for (let i = 0;i<content.length; i++){
+//     fs.unlinkSync("folder/"+content[i])  (delete file)
+// }
+
+//delete folder
+// fs.rmdirSync("folder") 
+//fs.existSync() -> reurn true if file exist otherwise false 
+//fs.lstateSync()-> tell is it is a file or directory (folder)
+
